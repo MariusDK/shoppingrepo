afed0c5fc85e63d14c595e530f88f90806392be8
Maierescu Marius <mariusdk7@gmail.com>
2018-08-20 13:18:23 +0300

    Update 1

--- app/src/main/java/com/example/marius/shoppingapp/classes/Item.java
+++ app/src/main/java/com/example/marius/shoppingapp/classes/Item.java
@@ -3,12 +3,12 @@ package com.example.marius.shoppingapp.classes;
 public class Item {
     public String name;
     public int quantity;
-    public int id_user;
+    public String id_user;
 
     public Item() {
     }
 
-    public Item(String name, int quantity, int id_user) {
+    public Item(String name, int quantity, String id_user) {
         this.name = name;
         this.quantity = quantity;
         this.id_user = id_user;
@@ -30,11 +30,11 @@ public class Item {
         this.quantity = quantity;
     }
 
-    public int getId_user() {
+    public String getId_user() {
         return id_user;
     }
 
-    public void setId_user(int id_user) {
+    public void setId_user(String id_user) {
         this.id_user = id_user;
     }
 }
