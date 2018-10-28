04a5c12aff0e9feb89a04dee68574c3d93e30672
MariusDK <mariusdk7@gmail.com>
2018-10-28 19:46:30 +0200

    Working Notification send and recive (making a service to recive using FirebaseMessagingService )

--- app/src/main/java/com/example/marius/shoppingapp/services/FriendRequestService.java
+++ app/src/main/java/com/example/marius/shoppingapp/services/FriendRequestService.java
@@ -1,9 +1,14 @@
 package com.example.marius.shoppingapp.services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.example.marius.shoppingapp.R;
 import com.example.marius.shoppingapp.activities.MainActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -15,12 +20,24 @@ public class FriendRequestService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
+
         if (remoteMessage.getData().size()>0)
         {
             Log.d(TAG,"Message data: "+ remoteMessage.getData());
         }
         if (remoteMessage.getNotification()!=null)
-        {createNotification(remoteMessage.getNotification().getBody());}
+        {
+            String messageTitle = remoteMessage.getNotification().getTitle();
+            String messageBody = remoteMessage.getNotification().getBody();
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, getString(R.string.default_notification_channel_id))
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(messageTitle)
+                    .setContentText(messageBody)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+            int notificationId = (int) System.currentTimeMillis();
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(notificationId,mBuilder.build());
+        }
     }
     public void createNotification(String body)
     {
